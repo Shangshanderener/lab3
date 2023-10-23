2d80bffa368221dad9feb8f57aa87f43c9df376c
Shangshanderener <hanzicheng0717@gmail.com>
2023-10-23 11:38:34 -0700

    Update list_test files

---
+++ ListTests.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+interface StringChecker { boolean checkString(String s); }
+
+
+class Checker implements StringChecker {
+    public boolean checkString(String s) {
+        return s.length() > 3;
+    }
+}
+
+public class ListTests{
+	@Test 
+	public void testFilter() {
+    List<String> input1 = new ArrayList<>(Arrays.asList ());
+    List<String> input2 = new ArrayList<>(Arrays.asList ("xyz", "abc"));
+    List<String> input3 = new ArrayList<>(Arrays.asList ("xyz", "abc"));
+    List<String> input4 = new ArrayList<>(Arrays.asList ("xyz", "abc"));
+    Checker string_checker = new Checker();
+    assertArrayEquals(new ArrayList<>(Arrays.asList ("xyz", "abc")), ListExamples.filter(input1, string_checker));
+	}
+
+
+  @Test
+  public void testMerge() {
+    int[] input1 = { };
+    int[] input2 = { 1, 2, 3 };
+    int[] input3 = { 3, 3 };
+    int[] input4 = { 1, 3, 2, 4 };
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input2));
+    assertArrayEquals(new int[]{ 3, 3 }, ArrayExamples.reversed(input3));
+    assertArrayEquals(new int[]{ 4, 2, 3, 1 }, ArrayExamples.reversed(input4));
+  }
+}
